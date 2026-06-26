56136be87ce19b8eb50493c5f6bf3c340f8bf998
Arthur <arthurefgg63@gmail.com>
2026-06-26 09:10:54 -0300

    Update siteconfig.js

--- siteconfig.js
+++ siteconfig.js
@@ -1,1 +1,11 @@
-
+window.GIFTHUB_CONFIG = Object.freeze({
+  apiTimeoutMs: 12000,
+  cacheTtlMs: 5 * 60 * 1000,
+  brandName: "GiftHub",
+  brandSubtitle: "Roblox analyzer público",
+  assets: Object.freeze({
+    kids: "assets/logo-kids.jpg",
+    select: "assets/logo-select.jpg",
+    blue: "assets/logo-blue.jpg"
+  })
+});
